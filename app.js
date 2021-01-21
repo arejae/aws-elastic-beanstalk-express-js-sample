36b434793c9174c4c95d855f4486dc6f31559cb9
Azam <noorazam@hla-group.com>
2021-01-21 15:56:20 +0800

    update hello rj

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello RJ! Test 1'));
+app.get('/', (req, res) => res.send('Hello RJ! Test 1 <br> Test 2 <h2>Amazon Web Service</h2>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
